f761fd6fe3537447192034bd9ab3f9ab1fa90acb
AlexHM <alex13pks@gmail.com>
2022-10-19 23:18:07 -0500

    Added description and logo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#091128',
+        primary: '#9A3D42',
+        secondary: '#AC3B40',
+        accent: '#CB5950',
+        light: '#EFEEEB',
+      },
+    },
+  },
+  plugins: [],
+};
