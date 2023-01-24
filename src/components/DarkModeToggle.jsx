ec5600a5b5519552ab0ed6a6a9b199695a27c61a
AlexHM <alex13pks@gmail.com>
2023-01-24 00:03:45 -0600

    Added darkmod toggle

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,26 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { LineMdLightDarkLoop } from './icons/LineMdLightDarkLoop';
+import React, { useState, useEffect, useRef } from 'react';
+import '../styles/switch.css';
 
 const DarkModeToggle = () => {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ?? 'dark');
-
+  const toggleRef = useRef(null);
   useEffect(() => {
+    console.log(theme);
     if (theme === 'light') {
       document.documentElement.classList.remove('dark');
+      toggleRef.current.checked = false;
     } else {
       document.documentElement.classList.add('dark');
+      toggleRef.current.checked = true;
     }
     localStorage.setItem('theme', theme);
   }, [theme]);
 
   const handleClick = () => {
+    console.log('change');
     setTheme(theme === 'dark' ? 'light' : 'dark');
   };
 
   return (
-    <button className="rounded-full  " onClick={handleClick}>
-      <LineMdLightDarkLoop width="24px" height="24px" />
-    </button>
+    <label className="switch">
+      <input type="checkbox" onChange={handleClick} ref={toggleRef} />
+      <span className="slider"></span>
+    </label>
   );
 };
 
