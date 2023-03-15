686520fbde3273ac9149786114ef002d7983bb5c
AlexHM <alex13pks@gmail.com>
2023-03-15 00:41:22 -0600

    Add projects page and separate page in components

---
+++ src/content/projects.js
@@ -0,0 +1,47 @@
+export const projects = [
+  /* {
+    name: '',
+    repo: '',
+    description: '',
+    technologies: [''],
+  }, */
+  {
+    name: 'DritsJS',
+    repo: 'github.com',
+    description: 'My own JavaScript sandbox! on windows and web',
+    technologies: ['TailwindCSS', 'Electron', 'JavaScript', 'Vite'],
+  },
+  {
+    name: 'Startupmate',
+    repo: 'github.com',
+    description: 'This is a short description about the project',
+    technologies: ['TailwindCSS', 'Remix', 'Cohere', 'TypeScript'],
+  },
+  {
+    name: 'Drits Wordle',
+    repo: 'github.com',
+    description: 'My own clone of wordle',
+    technologies: ['CSS', 'HTML', 'JavaScript', 'RxJS'],
+  },
+  {
+    name: 'Spotify Loop & Mood',
+    repo: 'github.com',
+    description: 'This is a short description about the project',
+    technologies: ['Nextjs', 'TailwindCSS', 'JavaScript'],
+  },
+  {
+    name: 'KDT POS',
+    repo: '',
+    description:
+      'MEAN stack app for a local bakery, compiled to desktop and web',
+    technologies: [
+      'Angular',
+      'Electron',
+      'SASS',
+      'TypeScript',
+      'NodeJs',
+      'Express',
+      'MongoDB',
+    ],
+  },
+];
