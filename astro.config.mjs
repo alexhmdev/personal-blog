50ffbb77447e08035ffe2177da3f94db38197643
AlexHM <alex13pks@gmail.com>
2024-05-09 21:10:01 -0600

    feat(ga): add google analytics 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 // https://astro.build/config
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import image from '@astrojs/image';
+import react from '@astrojs/react';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
-  site: "https://alexhm.dev/",
-  integrations: [tailwind(), mdx(), image(), react()],
+  site: 'https://alexhm.dev/',
+  integrations: [
+    tailwind(),
+    mdx(),
+    image(),
+    react(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
